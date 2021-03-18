ff72e9b41d037eeb335fe4497170f533e96a8f39
harshitb10 <harshit.bisht.22@gmail.com>
2021-03-18 15:38:09 +0530

    what I changed

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,10 @@
-import { useState, useRef } from "react";
+import { useState, useRef, React } from "react";
 import song from "../Songs/1.mp3";
 import web from "../images/img9.jpg";
 
 function Player() {
-  
   const [isPlaying, setIsPlaying] = useState(false);
-  
+
   const [currentTime, setCurrentTime] = useState(0);
 
   const audioRef = useRef();
@@ -22,6 +21,7 @@ function Player() {
     if (isPlaying) {
       setIsPlaying(false);
       audio.pause();
+      localStorage.setItem("data", JSON.stringify(secondsToHms(currentTime)));
     }
   };
 
@@ -30,6 +30,42 @@ function Player() {
 
     setCurrentTime(time.toFixed(2));
   };
+
+  //Bookmark function
+
+  const storage = localStorage.getItem(
+    "data",
+    JSON.stringify(secondsToHms(currentTime))
+  );
+  console.log(storage);
+
+  function secondsToHms(seconds) {
+    if (!seconds) return "00m 00s";
+
+    let duration = seconds;
+    let hours = duration / 3600;
+    duration = duration % 3600;
+
+    let min = parseInt(duration / 60);
+    duration = duration % 60;
+
+    let sec = parseInt(duration);
+
+    if (sec < 10) {
+      sec = `0${sec}`;
+    }
+    if (min < 10) {
+      min = `0${min}`;
+    }
+
+    if (parseInt(hours, 10) > 0) {
+      return `${parseInt(hours, 10)}h ${min}m ${sec}s`;
+    } else if (min == 0) {
+      return `00m ${sec}s`;
+    } else {
+      return `${min}m ${sec}s`;
+    }
+  }
 
   return (
     <div className="player">
@@ -44,13 +80,14 @@ function Player() {
         src={song}
         controls
       ></audio>
-      <div>
-        <button onClick={play}> paly /pause </button>
-
-        currentTime={currentTime}
-            
-      </div>
       
+      <div>
+        
+        <button onClick={play}> paly /pause </button>
+        {/* <button onClick = {click}>Click </button>  */}
+        <div > <p1> Bookmark {storage}</p1> </div>
+      </div>
+
       <div>
         <a href={song} download="1.mp3">
           {" "}
