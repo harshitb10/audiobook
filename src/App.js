eecb0e0438acea103e4e865c7fd6df975f7126c1
harshitb10 <harshit.bisht.22@gmail.com>
2021-03-19 20:35:06 +0530

    what I changed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 
 
+
 function App () {
 
   const [songs] = useState ([
--- src/components/Player.js
+++ src/components/Player.js
@@ -4,68 +4,58 @@ import web from "../images/img9.jpg";
 
 function Player() {
   const [isPlaying, setIsPlaying] = useState(false);
-
-  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0)
+ 
 
   const audioRef = useRef();
 
+ 
   const play = () => {
-    const audio = audioRef.current;
-    audio.volume = 0.1;
+    const audio = audioRef.current
+    audio.volume = 0.1
 
     if (!isPlaying) {
-      setIsPlaying(true);
-      audio.play();
+      setIsPlaying(true)
+       audio.play();
+      
     }
 
     if (isPlaying) {
-      setIsPlaying(false);
-      audio.pause();
-      localStorage.setItem("data", JSON.stringify(secondsToHms(currentTime)));
-    }
-  };
-
-  const getCurrDuration = (e) => {
-    const time = e.currentTarget.currentTime;
-
-    setCurrentTime(time.toFixed(2));
-  };
-
-  //Bookmark function
-
-  const storage = localStorage.getItem(
-    "data",
-    JSON.stringify(secondsToHms(currentTime))
-  );
-  console.log(storage);
-
-  function secondsToHms(seconds) {
-    if (!seconds) return "00m 00s";
-
-    let duration = seconds;
-    let hours = duration / 3600;
-    duration = duration % 3600;
-
-    let min = parseInt(duration / 60);
-    duration = duration % 60;
-
-    let sec = parseInt(duration);
-
-    if (sec < 10) {
-      sec = `0${sec}`;
-    }
-    if (min < 10) {
-      min = `0${min}`;
-    }
-
-    if (parseInt(hours, 10) > 0) {
-      return `${parseInt(hours, 10)}h ${min}m ${sec}s`;
-    } else if (min == 0) {
-      return `00m ${sec}s`;
-    } else {
-      return `${min}m ${sec}s`;
+      setIsPlaying(false)
+      audio.pause()
+      localStorage.setItem("data", JSON.stringify((currentTime)));
     }
   }
+  
+  
+
+
+  const getCurrDuration =  (e) => {
+    
+    const time = e.currentTarget.currentTime
+    
+    setCurrentTime(time) 
+    
+    }
+
+
+
+ let storage = localStorage.getItem("data",JSON.stringify((currentTime))
+);
+  console.log(storage);
+
+  
+  const Bookmark   = () => {
+    let storage = localStorage.getItem("data",JSON.stringify((currentTime))
+);
+  console.log(storage);
+    const audio = audioRef.current
+    audio.currentTime = storage
+    audio.play();
+  }
+
+
 
   return (
     <div className="player">
@@ -74,20 +64,27 @@ function Player() {
         <img src={web} />
       </div>
       <audio
+      controls
         className="player_audio"
         ref={audioRef}
-        onTimeUpdate={getCurrDuration}
+        onTimeUpdate = {getCurrDuration}
+        onLoadedData={(e) => {
+          setDuration(e.currentTarget.duration)
+        }}
         src={song}
-        controls
+       
       ></audio>
-      
+     
       <div>
         
         <button onClick={play}> paly /pause </button>
-        {/* <button onClick = {click}>Click </button>  */}
+        <button onClick={Bookmark}> bookmark </button>
+        
         <div > <p1> Bookmark {storage}</p1> </div>
       </div>
-
+      <div className='timer'>{(currentTime)}</div>
+      <div className='timer'>{(duration)}</div>
+      
       <div>
         <a href={song} download="1.mp3">
           {" "}
